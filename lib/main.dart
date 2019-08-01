123b9c37377ed458c003fe714f140c2a1c48cede
jerseysu <loverjersey@gmail.com>
2019-08-01 23:24:56 +0800

    feat: add 2nd SecondRoute

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class TutorialHome extends StatelessWidget {
           tooltip: 'Navigation menu',
           onPressed: null,
         ),
-        title: Text('Jeresy Cool!', key: Key('Title')),
+        title: Text('Jeresy Cool!', key: Key('firstTabTitle')),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
@@ -29,12 +29,58 @@ class TutorialHome extends StatelessWidget {
       ),
       // body is the majority of the screen.
       body: Center(
-        child: Text('Hello, world!', key: Key('TextField')),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:[
+            Text('Hello, Jersey!', 
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                  fontFamily: 'Roboto',
+                  letterSpacing: 0.5,
+                  fontSize: 30,
+                ),
+              key: Key('textField')),
+            FloatingActionButton(
+              tooltip: 'Add', // used by assistive technologies
+              child: Icon(Icons.add, key: Key('addIcon')),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                );
+              }
+            ),
+            RaisedButton(
+              child: Text('Open Second Tab', key: Key('openBtn')),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                );
+              },
+            ),
+          ]
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Add', // used by assistive technologies
-        child: Icon(Icons.add),
-        onPressed: null,
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jersey Second Tab", key: Key('secendTabTitle')),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!', key: Key('goBackBtn')),
+        ),
       ),
     );
   }
