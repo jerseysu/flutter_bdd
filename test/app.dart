9301668ec4a71860cd13cbfc64dd8824488ba80a
jerseysu <loverjersey@gmail.com>
2019-09-14 16:36:21 +0800

    fix: adjust package name

--- test/app.dart
+++ test/app.dart
@@ -1,5 +1,5 @@
 // ignore: avoid_relative_lib_imports
-import 'package:hello_world/main.dart' as hello_world;
+import 'package:flutter_bdd/main.dart' as flutter_bdd;
 import 'package:flutter_driver/driver_extension.dart';
 
 void main() {
@@ -8,5 +8,5 @@ void main() {
 
   // Call the `main()` function of your app or call `runApp` with any widget you
   // are interested in testing.
-  hello_world.main(); 
+  flutter_bdd.main(); 
 }
